88c4aa7910f1bdb6dd149e7a7e932932471ae5ed
wfeng66 <47467836+wfeng66@users.noreply.github.com>
2020-09-17 04:09:10 -0700

    Create CommonPython.py
    This file includes common snippets or tricks in python, for example, input, output, comprehension, and so on.

---
+++ CommonPython.py
@@ -0,0 +1,4 @@
+# concise input methods
+pos = [int(i) for i in input().strip().split()]
+dim = [int(i) for i in input().strip().split()]
+board = [[j for j in input().strip()] for i in range(dim[0])]
